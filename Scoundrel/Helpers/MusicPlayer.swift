671a274ec603af033fe62effb3c12992993eb231
David Freeman <david@d3.codes>
2025-03-05 15:21:59 -0600

    fixed bug that would prevent music from resuming after interuption

--- Scoundrel/Helpers/MusicPlayer.swift
+++ Scoundrel/Helpers/MusicPlayer.swift
@@ -33,8 +33,8 @@ class MusicPlayer: NSObject, ObservableObject, AVAudioPlayerDelegate {
     override init() {
         try? AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.ambient)
         try? AVAudioSession.sharedInstance().setActive(true)
-        
         super.init()
+        setupNotifications()
     }
     
     func playRandomSong() {
@@ -70,5 +70,39 @@ class MusicPlayer: NSObject, ObservableObject, AVAudioPlayerDelegate {
 
     func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
         nextTrack()
+    }
+    
+    func setupNotifications() {
+        // Get the default notification center instance.
+        let nc = NotificationCenter.default
+        nc.addObserver(self,
+                       selector: #selector(handleInterruption),
+                       name: AVAudioSession.interruptionNotification,
+                       object: AVAudioSession.sharedInstance())
+    }
+    
+    @objc func handleInterruption(notification: Notification) {
+        guard let userInfo = notification.userInfo,
+            let typeValue = userInfo[AVAudioSessionInterruptionTypeKey] as? UInt,
+            let type = AVAudioSession.InterruptionType(rawValue: typeValue) else {
+                return
+        }
+
+
+        // Switch over the interruption type.
+        switch type {
+        case .began: // An interruption began. Update the UI as necessary.
+            break
+        case .ended: // An interruption ended. Resume playback, if appropriate.
+            guard let optionsValue = userInfo[AVAudioSessionInterruptionOptionKey] as? UInt else { return }
+            let options = AVAudioSession.InterruptionOptions(rawValue: optionsValue)
+            if options.contains(.shouldResume) {
+                // An interruption ended. Resume playback.
+                music.play()
+            } else {
+                // An interruption ended. Don't resume playback.
+            }
+        default: ()
+        }
     }
 }
